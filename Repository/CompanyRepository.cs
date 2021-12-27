ef3ea93149b80e2a87a4032a6bd8bd9c2197c40d
Robiul <victorshakil9102@gmail.com>
2021-12-28 03:12:26 +0600

    add onion architecture

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Contracts;
+using Entities.Model;
+
+namespace Repository;
+
+public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+{
+    public CompanyRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    {
+    }
+}
---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Contracts;
+using Entities.Model;
+
+namespace Repository;
+
+public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+{
+    public EmployeeRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+    {
+    }
+}
